e603367a328ea83e1633bd60534c90ad349a7c1e
Michael Orchard <michael.orchard@live.co.uk>
2026-07-24 13:41:15 +0100

    feat: Adjust and log nuclear reactor boost

--- Assets/Scripts/Gameplay/NuclearReactor.cs
+++ Assets/Scripts/Gameplay/NuclearReactor.cs
@@ -3,14 +3,16 @@ using UnityEngine;
 public class NuclearReactor : MonoBehaviour
 {
     [SerializeField] private float BoostValue = 0;
-    [SerializeField] private float BoostMultiplier = 2;
+    [SerializeField] private float BoostMultiplier = 1.2f;
 
     public void OnWeld(Piece target)
     {
         if (target.TryGetComponent<EngineThrustEffect>(out var thrustEffect))
         {
-            thrustEffect.Thrust += BoostValue;
-            thrustEffect.Thrust *= BoostMultiplier;
+            var currentThrust = thrustEffect.Thrust;
+            var newThrust = (currentThrust * BoostMultiplier) + BoostValue;
+            Debug.Log("Applying thruster boost ((" + currentThrust + " * " + BoostMultiplier + ") + " + BoostValue + ") = " + newThrust);
+            thrustEffect.Thrust = newThrust;
         }
     }
 }
