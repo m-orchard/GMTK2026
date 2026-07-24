fafe785bcf7ba0bd55e70540395c3a07709dc4a2
Andrew Jones <andrew_nomie@msn.com>
2026-07-24 10:18:41 +0100

    Prefabify

--- Assets/Scripts/Gameplay/PieceSpawner.cs
+++ Assets/Scripts/Gameplay/PieceSpawner.cs
@@ -1,15 +1,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PieceSpawner : MonoBehaviour
-{
+public class PieceSpawner : Singleton<PieceSpawner> {
     [SerializeField] private Transform spawnPoint;
     [SerializeField] private RocketAssembly rocket;
     [SerializeField] private GameObject bodyPrefab;
     [SerializeField] private GameObject enginePrefab;
     [SerializeField] private GameObject cargoPrefab;
+
     [Range(0f, 1f)]
     [SerializeField] private float engineChance = 0.3f;
+
     [SerializeField] private int bagSize = 7;
     [SerializeField] private float wellMinX = -2f;
     [SerializeField] private float wellMaxX = 2f;
@@ -19,35 +20,32 @@ public class PieceSpawner : MonoBehaviour
     public FallingPieceController Active { get; private set; }
     public bool HasCargoBeenDropped { get; private set; }
 
-    public void SpawnNext()
-    {
+    public void SpawnNext() {
         SpawnPiece(NextFromBag());
     }
 
-    public void SpawnCargo()
-    {
-        if (HasCargoBeenDropped) return;
-        if (Active != null) DiscardActive();
+    public void SpawnCargo() {
+        if (HasCargoBeenDropped)
+            return;
+        if (Active != null)
+            DiscardActive();
 
         HasCargoBeenDropped = true;
         var controller = SpawnPiece(cargoPrefab);
         rocket.SetCargoPiece(controller.GetComponent<Piece>());
     }
 
-    private void DiscardActive()
-    {
+    private void DiscardActive() {
         Active.OnReleased -= HandleReleased;
         Destroy(Active.gameObject);
         Active = null;
     }
 
-    public void ResetCargo()
-    {
+    public void ResetCargo() {
         HasCargoBeenDropped = false;
     }
 
-    private FallingPieceController SpawnPiece(GameObject prefab)
-    {
+    private FallingPieceController SpawnPiece(GameObject prefab) {
         GameObject instance = Instantiate(prefab, spawnPoint.position, Quaternion.identity, rocket.transform);
 
         var controller = instance.GetComponent<FallingPieceController>();
@@ -59,24 +57,23 @@ public class PieceSpawner : MonoBehaviour
         return controller;
     }
 
-    private void HandleReleased()
-    {
+    private void HandleReleased() {
         Active.OnReleased -= HandleReleased;
         Active = null;
         SpawnNext();
     }
 
-    public void ForceLockActive()
-    {
-        if (Active == null) return;
+    public void ForceLockActive() {
+        if (Active == null)
+            return;
         Active.OnReleased -= HandleReleased;
         Active.ForceLock();
         Active = null;
     }
 
-    private GameObject NextFromBag()
-    {
-        if (bag.Count == 0) RefillBag();
+    private GameObject NextFromBag() {
+        if (bag.Count == 0)
+            RefillBag();
 
         int lastIndex = bag.Count - 1;
         GameObject prefab = bag[lastIndex];
@@ -84,17 +81,17 @@ public class PieceSpawner : MonoBehaviour
         return prefab;
     }
 
-    private void RefillBag()
-    {
+    private void RefillBag() {
         int engineCount = Mathf.Clamp(Mathf.RoundToInt(bagSize * engineChance), 1, bagSize);
         int bodyCount = bagSize - engineCount;
 
         bag.Clear();
-        for (int i = 0; i < engineCount; i++) bag.Add(enginePrefab);
-        for (int i = 0; i < bodyCount; i++) bag.Add(bodyPrefab);
+        for (int i = 0; i < engineCount; i++)
+            bag.Add(enginePrefab);
+        for (int i = 0; i < bodyCount; i++)
+            bag.Add(bodyPrefab);
 
-        for (int i = bag.Count - 1; i > 0; i--)
-        {
+        for (int i = bag.Count - 1; i > 0; i--) {
             int j = Random.Range(0, i + 1);
             (bag[i], bag[j]) = (bag[j], bag[i]);
         }
