f68c4aeb3be50491863e3cb1c04a436b27fbc854
Andrew Jones <andrew_nomie@msn.com>
2026-07-26 13:10:12 +0100

    Fix text

--- Assets/Scripts/Gameplay/Rewards/CardSelectionManager.cs
+++ Assets/Scripts/Gameplay/Rewards/CardSelectionManager.cs
@@ -3,9 +3,12 @@ using UnityEngine;
 public class CardSelectionManager : MonoBehaviour
 {
     [SerializeField] private CardProvider cardProvider;
+    [SerializeField] private RewardIntroBanner bannerPrefab;
+    [SerializeField] private Transform bannerContainer;
     [SerializeField] private PieceCardView cardPrefab;
     [SerializeField] private Transform cardContainer;
 
+    private RewardIntroBanner activeBanner;
     private PieceCardView activeCard;
     private PieceCard offeredCard;
 
@@ -19,7 +22,8 @@ public class CardSelectionManager : MonoBehaviour
         if (GameManager.Instance != null)
             GameManager.Instance.OnRoundResult -= HandleRoundResult;
 
-        DespawnActiveCard();
+        DespawnCard();
+        DespawnBanner();
     }
 
     private void HandleRoundResult(float apex, float targetHeight, bool success)
@@ -35,6 +39,15 @@ public class CardSelectionManager : MonoBehaviour
             return;
         }
 
+        activeBanner = Instantiate(bannerPrefab, bannerContainer);
+        activeBanner.IntroCompleted += ShowCard;
+    }
+
+    private void ShowCard()
+    {
+        if (activeBanner != null)
+            activeBanner.IntroCompleted -= ShowCard;
+
         activeCard = Instantiate(cardPrefab, cardContainer);
         activeCard.Populate(offeredCard);
         activeCard.Clicked += HandleCardPicked;
@@ -45,11 +58,12 @@ public class CardSelectionManager : MonoBehaviour
         LevelManager.Instance.AcquirePiece(offeredCard.PiecePrefab);
         offeredCard = null;
 
-        DespawnActiveCard();
+        DespawnCard();
+        DismissBanner();
         GameManager.Instance.GoToNextLevel();
     }
 
-    private void DespawnActiveCard()
+    private void DespawnCard()
     {
         if (activeCard == null)
             return;
@@ -57,5 +71,25 @@ public class CardSelectionManager : MonoBehaviour
         activeCard.Clicked -= HandleCardPicked;
         Destroy(activeCard.gameObject);
         activeCard = null;
+    }
+
+    private void DismissBanner()
+    {
+        if (activeBanner == null)
+            return;
+
+        activeBanner.IntroCompleted -= ShowCard;
+        activeBanner.Dismiss();
+        activeBanner = null;
+    }
+
+    private void DespawnBanner()
+    {
+        if (activeBanner == null)
+            return;
+
+        activeBanner.IntroCompleted -= ShowCard;
+        Destroy(activeBanner.gameObject);
+        activeBanner = null;
     }
 }
